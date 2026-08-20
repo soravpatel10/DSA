fcc26ccb3b254d7cb2e699dfeb525fef9602b566
soravpatel10 <soravpatel10@gmail.com>
2026-08-20 10:51:29 +0530

    Time: 1 ms (95.69%) | Memory: 43.8 MB (11.89%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        return isSub(s,t,0,0);
+        }
+
+        public boolean isSub(String s, String t, int i , int j){
+            if (i==s.length()){
+                return true;
+            }
+
+            if(j==t.length()){
+                return false;
+            }
+
+            if( s.charAt(i) != t.charAt(j)){
+                return isSub(s,t,i,j+1);
+            }
+
+            if( s.charAt(i)==t.charAt(j)){
+                return isSub(s,t,i+1,j+1);
+            }
+            return false;
+    }
+}
