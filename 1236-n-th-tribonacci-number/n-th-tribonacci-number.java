c8b97ed7168f7ae3389cc68ef000ada116c2b39c
soravpatel10 <soravpatel10@gmail.com>
2026-08-25 11:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (71.82%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -53,25 +53,3 @@ class Solution{
         return dp.get(n);
     }
 }
-
-
-/*class Solution{
-    public int fib(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        ArrayList<Integer> dp = new ArrayList<>();
-
-        for (int i = 0; i <= n; i++) {
-            dp.add(0);
-        }
-
-    dp.set(0 ,0);
-    dp.set(1, 1);
-
-    for(int i=2; i<=n; i++){
-        dp.set(i, dp.get(i-1) + dp.get(i-2));
-    }
-    return dp.get(n);
-}
-}*/
