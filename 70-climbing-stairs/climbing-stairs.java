298dffd5c68f9a11f3e6632830ff97f6e1175cde
soravpatel10 <soravpatel10@gmail.com>
2026-08-29 17:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,23 +1,43 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         ArrayList<Integer> dp = new ArrayList<>();
+//         for(int i=0; i<=n; i++){
+//             dp.add(-1);
+//         }
+//         return solve(n, dp);
+//     }
+
+//     private int solve (int n, ArrayList<Integer> dp){
+//         if(n==0 || n==1){
+//             return 1;
+//         }
+
+//         if(dp.get(n) != -1){
+//             return dp.get(n);
+//         }
+//         dp.set(n, solve(n-1, dp) + solve(n-2,dp));
+//         return dp.get(n);
+//     }
+// }
+
 class Solution {
     public int climbStairs(int n) {
         ArrayList<Integer> dp = new ArrayList<>();
-        for(int i=0; i<=n; i++){
+        for(int i=0; i<=n ; i++){
             dp.add(-1);
         }
+
         return solve(n, dp);
     }
 
-    private int solve (int n, ArrayList<Integer> dp){
-        if(n==0 || n==1){
-            return 1;
-        }
-
+    public int solve (int n, ArrayList<Integer> dp){
+        if(n<=1) return 1;
         if(dp.get(n) != -1){
             return dp.get(n);
         }
-        dp.set(n, solve(n-1, dp) + solve(n-2,dp));
+
+        dp.set(n, solve(n-1 , dp) + solve(n-2, dp));
         return dp.get(n);
     }
 }
-
 
