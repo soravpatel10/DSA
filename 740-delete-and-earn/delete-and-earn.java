c9d289e2edeec4b4ef9a7e166e164a7ed2c5cc98
soravpatel10 <soravpatel10@gmail.com>
2026-09-02 00:16:05 +0530

    Time: 4 ms (43.92%) | Memory: 47.6 MB (8.54%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,48 +1,42 @@
 class Solution {
-
     public int deleteAndEarn(int[] nums) {
 
         int max = 0;
-
-        for (int num : nums) {
-            max = Math.max(max, num);
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
         }
 
         int[] freq = new int[max + 1];
-
-        for (int num : nums) {
-            freq[num]++;
+        for(int i = 0; i < nums.length; i++){
+            freq[nums[i]]++;
         }
-
         ArrayList<Integer> dp = new ArrayList<>();
 
-        for (int i = 0; i <= max; i++) {
+        for(int i = 0; i <= max; i++){
             dp.add(-1);
         }
-
         return solve(max, freq, dp);
     }
 
-    public int solve(int i, int[] freq, ArrayList<Integer> dp) {
+    public int solve(int i, int[] freq, ArrayList<Integer> dp){
 
-        if (i == 0) {
+        if(i == 0){
             return 0;
         }
 
-        if (i == 1) {
+        if(i == 1){
             return freq[1];
         }
 
-        if (dp.get(i) != -1) {
+        if(dp.get(i) != -1){
             return dp.get(i);
         }
 
         int notTake = solve(i - 1, freq, dp);
-
         int take = (i * freq[i]) + solve(i - 2, freq, dp);
-
         dp.set(i, Math.max(take, notTake));
-
         return dp.get(i);
     }
 }
