11d2a5cf6f4f9f89879ab609bc23a35d7ff2d57a
soravpatel10 <soravpatel10@gmail.com>
2026-09-09 23:34:43 +0530

    Time: 13 ms (15.55%) | Memory: 47.6 MB (10.13%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,42 +1,62 @@
+import java.util.*;
+
 class Solution {
-    public int deleteAndEarn(int[] nums) {
 
-        int max = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > max){
-                max = nums[i];
-            }
+    int[] dp = new int[20005];
+
+    int fun(int i, ArrayList<Integer> nums, HashMap<Integer, Integer> map) {
+
+        if (i == nums.size() - 1) {
+            return nums.get(i) * map.get(nums.get(i));
         }
 
-        int[] freq = new int[max + 1];
-        for(int i = 0; i < nums.length; i++){
-            freq[nums[i]]++;
-        }
-        ArrayList<Integer> dp = new ArrayList<>();
-
-        for(int i = 0; i <= max; i++){
-            dp.add(-1);
-        }
-        return solve(max, freq, dp);
-    }
-
-    public int solve(int i, int[] freq, ArrayList<Integer> dp){
-
-        if(i == 0){
+        if (i >= nums.size()) {
             return 0;
         }
 
-        if(i == 1){
-            return freq[1];
+        if (dp[i] != -1) {
+            return dp[i];
         }
 
-        if(dp.get(i) != -1){
-            return dp.get(i);
+        int max = 0;
+
+        if (i + 1 < nums.size() &&
+            nums.get(i + 1) == nums.get(i) + 1) {
+
+            int a = (nums.get(i) * map.get(nums.get(i)))
+                    + fun(i + 2, nums, map);
+
+            max = Math.max(max, a);
         }
 
-        int notTake = solve(i - 1, freq, dp);
-        int take = (i * freq[i]) + solve(i - 2, freq, dp);
-        dp.set(i, Math.max(take, notTake));
-        return dp.get(i);
+        if (i + 1 < nums.size() &&
+            nums.get(i + 1) != nums.get(i) + 1) {
+
+            int a = (nums.get(i) * map.get(nums.get(i)))
+                    + fun(i + 1, nums, map);
+
+            max = Math.max(max, a);
+        }
+
+        int a = fun(i + 1, nums, map);
+        max = Math.max(max, a);
+
+        return dp[i] = max;
+    }
+
+    public int deleteAndEarn(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        ArrayList<Integer> v1 = new ArrayList<>(map.keySet());
+        Collections.sort(v1);
+
+        Arrays.fill(dp, -1);
+
+        return fun(0, v1, map);
     }
 }
