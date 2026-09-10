d56a2c14702f94be6890b5c43f4d34238183440a
soravpatel10 <soravpatel10@gmail.com>
2026-09-10 12:33:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (28.46%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,36 +1,69 @@
-//Memoization
+// //Memoization
+// class Solution {
+//     ArrayList<ArrayList<Integer>> dp;
+//     public int uniquePaths(int m, int n) {
+
+//         dp = new ArrayList<>();
+//         for (int i = 0; i < m; i++) {
+//             ArrayList<Integer> row = new ArrayList<>();
+
+//             for (int j = 0; j < n; j++){
+//                 row.add(-1);
+//             }
+//             dp.add(row);
+//         }
+//         return solve(m - 1, n - 1);
+//     }
+
+//     public int solve(int m, int n) {
+
+//         if (m == 0 && n == 0)
+//             return 1;
+
+//         if (m < 0 || n < 0)
+//             return 0;
+
+//         if (dp.get(m).get(n) != -1)
+//             return dp.get(m).get(n);
+
+//         int up = solve(m - 1, n);
+//         int left = solve(m, n - 1);
+
+//         dp.get(m).set(n, up + left);
+        
+//         return dp.get(m).get(n);
+//     }
+// }
+
+//tabulation
 class Solution {
-    ArrayList<ArrayList<Integer>> dp;
+    int[][] dp;
     public int uniquePaths(int m, int n) {
+    dp = new int[m][n];
 
-        dp = new ArrayList<>();
         for (int i = 0; i < m; i++) {
-            ArrayList<Integer> row = new ArrayList<>();
-
-            for (int j = 0; j < n; j++){
-                row.add(-1);
-            }
-            dp.add(row);
+            Arrays.fill(dp[i], -1);
         }
         return solve(m - 1, n - 1);
     }
 
     public int solve(int m, int n) {
 
-        if (m == 0 && n == 0)
+        if (m == 0 && n == 0) {
             return 1;
+        }
 
-        if (m < 0 || n < 0)
+        if (m < 0 || n < 0) {
             return 0;
+        }
 
-        if (dp.get(m).get(n) != -1)
-            return dp.get(m).get(n);
+        if (dp[m][n] != -1) {
+            return dp[m][n];
+        }
 
         int up = solve(m - 1, n);
         int left = solve(m, n - 1);
 
-        dp.get(m).set(n, up + left);
-        
-        return dp.get(m).get(n);
+        return dp[m][n] = up + left;
     }
 }
