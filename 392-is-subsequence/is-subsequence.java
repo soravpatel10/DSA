c5ba4a4b0b4fa07c50396c59353c433c3e310541
soravpatel10 <soravpatel10@gmail.com>
2026-09-15 00:17:28 +0530

    Time: 4 ms (2.86%) | Memory: 43 MB (12.77%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,25 +1,53 @@
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         return isSub(s,t,0,0);
+//         }
+
+//         public boolean isSub(String s, String t, int i , int j){
+//             if (i==s.length()){
+//                 return true;
+//             }
+
+//             if(j==t.length()){
+//                 return false;
+//             }
+
+//             if( s.charAt(i) != t.charAt(j)){
+//                 return isSub(s,t,i,j+1);
+//             }
+
+//             if( s.charAt(i)==t.charAt(j)){
+//                 return isSub(s,t,i+1,j+1);
+//             }
+//             return false;
+//     }
+// }
+
+
+//tabulation
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        return isSub(s,t,0,0);
+
+        int m = s.length();
+        int n = t.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        for (int j = 0; j <= n; j++) {
+            dp[m][j] = true;
         }
 
-        public boolean isSub(String s, String t, int i , int j){
-            if (i==s.length()){
-                return true;
-            }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
 
-            if(j==t.length()){
-                return false;
+                if (s.charAt(i) == t.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = dp[i][j + 1];
+                }
             }
+        }
 
-            if( s.charAt(i) != t.charAt(j)){
-                return isSub(s,t,i,j+1);
-            }
-
-            if( s.charAt(i)==t.charAt(j)){
-                return isSub(s,t,i+1,j+1);
-            }
-            return false;
+        return dp[0][0];
     }
 }
-
